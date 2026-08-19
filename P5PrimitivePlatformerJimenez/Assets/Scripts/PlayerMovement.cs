d7cbda1889c6cbbe20f372c90d4c21efe6726d2a
J0EYJ0ES <garuj10@outlook.com>
2026-08-19 13:41:29 -0700

    added rigidbody and script
    added a scripts folder and added a rigidbody and set it to kinematic on the sphere.

---
+++ P5PrimitivePlatformerJimenez/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Private Variables
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public Camera mainCamera;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
